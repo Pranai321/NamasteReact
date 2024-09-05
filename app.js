af6d6dd4dcfde41629b45f89f7fbcee73676acba
kalva pranai sai reddy <kalvapranaisaireddy@gmail.com>
2024-09-05 14:56:28 -0500

    episode-01

---
+++ app.js
@@ -0,0 +1,15 @@
+
+const div = React.createElement("div", {id: "parent"}, [
+    React.createElement("div", "", [
+      React.createElement("h1", "", "I am an h1 tag"),
+      React.createElement("h2", "", "I am an h2 tag")
+    ]),
+    React.createElement("div","", [
+        React.createElement("h1", "", " I am an hh1 tag"),
+        React.createElement("h2", "", "I am an hh2 tag")
+    ])
+])
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(div);
+
